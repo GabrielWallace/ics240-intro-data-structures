572c55eaf09b657a69ebaf7682c3a88cce4e7229
Gabriel Wallace <gabriel.wallace.dev@gmail.com>
2017-06-10 00:26:21 -0500

    Add files via upload

--- P3_GAW886_Calculator/tests/edu/metrostate/edu/ics240/p3/gaw886/tests/calc/CalculatorTests.java
+++ P3_GAW886_Calculator/tests/edu/metrostate/edu/ics240/p3/gaw886/tests/calc/CalculatorTests.java
@@ -1,4 +1,5 @@
 package edu.metrostate.edu.ics240.p3.gaw886.tests.calc;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -8,15 +9,15 @@ import java.util.Random;
 import edu.metrostate.ics240.p3.gaw886.calc.Calculator;
 
 public class CalculatorTests {
-	static Random random = new Random();
+	static Random r = new Random();
 
 	@Test
 	public void testEnterMethod() {
 		Calculator calc = new Calculator();
 
-		//Add 100 random doubles to the stack
+		// Add 100 random doubles to the stack
 		for (int i = 0; i < 100; i++) {
-			calc.enter(new BigInteger(10, random).toString());
+			calc.enter(new BigInteger(10, r).toString());
 		}
 		assertEquals(100, calc.size());
 		calc.clear();
@@ -26,38 +27,62 @@ public class CalculatorTests {
 		calc.enter("10");
 		calc.enter("5");
 		calc.enter("+");
-		assertEquals(15, calc.peek(), 2);
+		assertEquals(15, calc.peek(), 20);
+
+		calc.clear();
+		calc.enter("2.5");
+		calc.enter("2.25");
+		calc.enter("+");
+		assertEquals(4.75, calc.peek(), 20);
 
 		// Test subtraction
 		calc.clear();
 		calc.enter("10");
 		calc.enter("5");
 		calc.enter("-");
-		assertEquals(5, calc.peek(), 2);
+		assertEquals(5, calc.peek(), 20);
+
+		calc.clear();
+		calc.enter("2");
+		calc.enter("1.25");
+		calc.enter("-");
+		assertEquals(0.75, calc.peek(), 20);
 
 		// Test multiplication
 		calc.clear();
 		calc.enter("10");
 		calc.enter("5");
 		calc.enter("*");
-		assertEquals(50, calc.peek(), 2);
+		assertEquals(50, calc.peek(), 20);
+
+		calc.clear();
+		calc.enter("2.5");
+		calc.enter("2.5");
+		calc.enter("*");
+		assertEquals(6.25, calc.peek(), 20);
 
 		// Test division
 		calc.clear();
 		calc.enter("10");
 		calc.enter("5");
 		calc.enter("/");
-		assertEquals(2, calc.peek(), 2);
+		assertEquals(2, calc.peek(), 20);
+
+		calc.clear();
+		calc.enter("4.5");
+		calc.enter("2.5");
+		calc.enter("/");
+		assertEquals(1.8, calc.peek(), 20);
 	}
-	
+
 	@Test
-	public void testEnterMethodSpecialCases(){
+	public void testEnterMethodSpecialCases() {
 		Calculator calc = new Calculator();
 		// Test single operator
 		calc.clear();
 		calc.enter("+");
 		assertEquals(0, calc.size());
-		
+
 		// Test single value/single operator addition
 		calc.clear();
 		calc.enter("5");
@@ -81,17 +106,17 @@ public class CalculatorTests {
 		calc.enter("5");
 		calc.enter("/");
 		assertEquals(1, calc.peek(), 2);
-		
-		//Test fail on division by 0
-		try {//Two distinct values
+
+		// Test fail on division by 0
+		try {// Two distinct values
 			calc.enter("2");
 			calc.enter("0");
 			calc.enter("/");
 		} catch (IllegalArgumentException iae) {
 			// expected
 		}
-		
-		try {//One distinct value
+
+		try {// One distinct value
 			calc.enter("0");
 			calc.enter("/");
 		} catch (IllegalArgumentException iae) {
